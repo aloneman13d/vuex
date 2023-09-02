d56c73e2ba7b161073ff82e3e7914fefba211f20
aloneman13d <aloneman13d@gmail.com>
2023-09-02 16:09:33 +0700

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <article class="md:container md:mx-auto bg-slate-600">
+    <header>
+      <h1>ทดลองใช้ vuex</h1>
+    </header>
+    <section>
+      <div>ค่าใน stor state count </div>
+    </section>
+  </article>
 </template>
 
+<script>
+export default {
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
